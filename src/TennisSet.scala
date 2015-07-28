b6466e6b8f47d0a028d96364ae60f2411dc666f3
Kushagra8888 <kushagra8888@gmail.com>
2015-07-28 17:12:53 +0530

    add tennisSet class

---
+++ src/TennisSet.scala
@@ -0,0 +1,17 @@
+/**
+ * Created by test on 7/28/2015.
+ */
+
+class TennisSet() {
+  var player1score: Int = 0
+  var player2score: Int = 0
+  var isMatchOngoing: Boolean = false
+  var isFinished: Boolean = false
+
+  def update( scoredBy: Char) = {
+    if (!isMatchOngoing) {
+
+    }
+  }
+
+}
